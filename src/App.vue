33f911de377bdec22b06152f48a23234668357b6
FoSuCloud <m13250117572@163.com>
2019-09-05 14:18:19 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus" rel="sheetsheet/stylus">
+  #app
+    color:$color-theme
+</style>
